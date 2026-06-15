8b898c3fc6c6d7477548d99153a9595584efa3c7
De'Jour Ford <dejourford@yahoo.com>
2026-06-15 08:20:45 -0500

    create variables for alb

---
+++ terraform/modules/alb/variables.tf
@@ -0,0 +1,7 @@
+variable "project" {type = string}
+variable "environment" {type = string}
+variable "alb_sg_id" {type = string}
+variable "public_subnet_a_id" {type = string}
+variable "public_subnet_b_id" {type = string}
+variable "certificate_arn" {type = string}
+variable "vpc_id" {type = string}
