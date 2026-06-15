561d8850cccbe838a9d8f0ff3321b7db9faf274e
De'Jour Ford <dejourford@yahoo.com>
2026-06-15 08:19:43 -0500

    create alb module

---
+++ terraform/modules/alb/main.tf
@@ -0,0 +1,60 @@
+# ALB
+resource "aws_lb" "this" {
+  name               = "${var.project}-${var.environment}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.alb_sg_id]
+  subnets            = [var.public_subnet_a_id, var.public_subnet_b_id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "${var.project}-${var.environment}-alb"
+    Environment = var.environment
+  }
+}
+
+# HTTPS LISTENER
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
+}
+
+# HTTP LISTENER (redirects to HTTPS)
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+# TARGET GROUP
+resource "aws_lb_target_group" "frontend" {
+  name        = "${var.project}-${var.environment}-frontend-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+}
