093013e76fb93e294fa75b16b34e83431d04932f
De'Jour Ford <dejourford@yahoo.com>
2026-06-15 01:29:13 -0500

    add variables

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,7 @@
+variable "project" {type = string}
+variable "environment" {type = environment}
+variable "vpc_cidr" {type = string}
+variable "public_a_cidr" {type = string}
+variable "public_b_cidr" {type = string}
+variable "private_a_cidr" {type = string}
+variable "private_b_cidr" {type = string}
