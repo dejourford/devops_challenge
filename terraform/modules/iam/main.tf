3fcc724f76b8a4be9494c06fd01289f80c4348f2
De'Jour Ford <dejourford@yahoo.com>
2026-06-15 19:20:02 -0500

    create iam resources

---
+++ terraform/modules/iam/main.tf
@@ -0,0 +1,48 @@
+# TASK EXECUTION ROLE
+resource "aws_iam_role" "task_execution" {
+  name = "${var.project}-${var.environment}-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project}-${var.environment}-task-execution-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "task_execution" {
+  role       = aws_iam_role.task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# TASK ROLE
+resource "aws_iam_role" "task" {
+  name = "${var.project}-${var.environment}-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project}-${var.environment}-task-role"
+  }
+}
